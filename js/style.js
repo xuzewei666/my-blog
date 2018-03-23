70ae548468033724a7193bd2be69fefae45bfa7d
xuzewei666 <32920553+xuzewei666@users.noreply.github.com>
2018-03-23 17:59:17 +0800

    little   修改 modify

--- js/style.js
+++ js/style.js
@@ -9,7 +9,7 @@ map.centerAndZoom(new BMap.Point(120.698506, 27.994322), 15); // 初始化地图
 map.addControl(new BMap.MapTypeControl()); //添加地图类型控件
 map.setCurrentCity("温州"); // 设置地图显示的城市 此项是必须设置的 
 map.enableScrollWheelZoom(true); //开启鼠标滚轮缩放
-var sContent = "地址：温州市 高教园区 温州商学院 2-717";
+var sContent = "地址：温州市 高教园区 温州商学院 2-719";
 //	var map = new BMap.Map("l-map");
 var point = new BMap.Point(120.698506, 27.924322);
 //	map.centerAndZoom(point, 15);
